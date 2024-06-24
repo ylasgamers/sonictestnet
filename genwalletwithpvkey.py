647f3f76e9b724135a50545a4d22599afb09cefd
ylasgamers <ylasgamers@gmail.com>
2024-06-25 00:47:37 +0700

    Update genwalletwithpvkey.py

--- genwalletwithpvkey.py
+++ genwalletwithpvkey.py
@@ -9,8 +9,8 @@ print("Auto Generate Wallet Solana With Privatekey | @ylasgamers")
 loop = input("How Many You Want To Generate Wallet ? : ")
 for i in range(0,int(loop)):
     keyreceiver = Keypair() #generate random privatekey
-    log('Address : '+keyreceiver.public_key) #log address
-    log('Privatekey : '+keyreceiver.private_key) #log privatekey
+    log('Address : '+str(keyreceiver.public_key)) #log address
+    log('Privatekey : '+str(keyreceiver.private_key)) #log privatekey
     log('-------------------'+str(i)+'-------------------')
     print('Address : ',keyreceiver.public_key) #print address
     print('Privatekey : ',keyreceiver.private_key) #print privatekey
